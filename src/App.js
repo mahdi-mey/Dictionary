0bba4c0b263cbc83b0c295f56dfd9cf65940b565
Mahdi <mahdimey17@gmail.com>
2024-02-19 23:15:30 +0330

    added fetch function & conditional rendering

--- src/App.js
+++ src/App.js
@@ -1,12 +1,39 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [inputValue, setInputValue] = useState('')
+  const [wordInfo, setWordInfo] = useState(null)
+  function handelChange(event) {
+    setInputValue(event.target.value)
+  }
+
+  async function searchWord() {
+    if (!inputValue) return
+    console.log('inside fetch function')
+    const response = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${inputValue}`)
+    const data = await response.json()
+    console.log(data)
+    setWordInfo(data)
+  }
+
   return (
-    <div class="container">
-      <div class="input-container">
-        <input type="text" name="" id="wordInp" placeholder="Enter A Word" spellcheck="false" value="hello" autofocus />
-          <button class="submitBtn" type="button" >Serach</button>
+    <div className="container">
+      <div className="input-container">
+        <input type="text" name="" id="wordInp" placeholder="Enter A Word" spellCheck="false" autoFocus onChange={handelChange} />
+        <button className="submitBtn" type="button" onClick={searchWord} >Serach</button>
       </div>
+
+      {wordInfo && <div className='main'>
+        <div className='information'>
+          <h1 className="selfWord">{wordInfo[0].word}</h1>
+          <div className="info">
+            <span className="span pron phonetic">{wordInfo[0].phonetics[1].text }</span>
+            <ion-icon name="volume-high-outline" className="audio pointer audio-icon"></ion-icon>
+            <audio src={wordInfo[0].phonetics[0].audio} id="audio"></audio>
+          </div>
+        </div>
+      </div>}
 
     </div>
   );
