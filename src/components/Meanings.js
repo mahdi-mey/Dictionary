92aa9ff56a6f0719bb623e1bac75665649cbcdde
Mahdi <mahdimey17@gmail.com>
2024-03-03 17:32:12 +0330

    rendered tabs with first one having active className

--- src/components/Meanings.js
+++ src/components/Meanings.js
@@ -1,13 +1,25 @@
+import { useState } from 'react'
 import './Meanings.css'
 
 export default function Meanings({ meanings }) {
-    console.log(meanings)
+    const [openTab, setOpenTab] = useState(1)
+    console.log(meanings[openTab].definitions[0].definition)
 
     return (
-        <div style={{ display: 'flex', gap: '1rem' }}>
+        <div style={{ display: 'flex', gap: '0.5rem' }}>
             {
-                meanings.map(item => {
-                    return <button className='tab-button'>{item.partOfSpeech}</button>
+                meanings.map((item, index) => {
+                    return (
+                        <div key={index}>
+                            <button className={`tab-button ${index === 0 ? 'active' : ''}`}>
+                                {item.partOfSpeech}
+                            </button>
+
+                            <div className='tab-content'>
+                                {/* {item.definitions[0].definition} */}
+                            </div>
+                        </div>
+                    )
                 })
             }
         </div>
