7dd9feae048cd695549adc758df3e3a313211b2f
Mahdi <mahdimey17@gmail.com>
2024-05-24 16:03:31 +0330

    add error handling and show message onerror

--- src/components/Dictionary.js
+++ src/components/Dictionary.js
@@ -10,32 +10,35 @@ export default function Dictionary() {
     const [inputValue, setInputValue] = useState('')
     const [wordInfo, setWordInfo] = useState(null)
     const [isLoading, setIsLoading] = useState(false)
+    const [errorMessage, setErrorMessage] = useState('');
 
     const audioRef = useRef(null)
 
-    function handelChange(event) {
+    function handleChange(event) {
         setInputValue(event.target.value)
     }
 
     async function searchWord(e) {
         e.preventDefault()
         if (!inputValue) return
-        
+
         try {
             setIsLoading(true)
+            setErrorMessage('')
             const response = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${inputValue}`)
             if (response.status === 200) {
                 const data = await response.json()
                 setWordInfo(data)
                 setInputValue('')
-                console.log(data)
             }
             else {
-                console.warn('some error happened', response.err)
+                // Handle errors
+                const errorData = await response.json()
+                setErrorMessage(errorData.message || 'An error occurred. Please try again later.');
             }
         }
         catch (err) {
-            console.log('catch block', err)
+            setErrorMessage('An error occurred. Please try again later.');
         }
         finally {
             setIsLoading(false)
@@ -51,17 +54,18 @@ export default function Dictionary() {
     return (
         <div className="container">
             <form className="input-container" onSubmit={searchWord}>
-                <input type="text" autoComplete="off" id="wordInp" placeholder="Enter A Word" spellCheck="false" autoFocus onChange={handelChange} />
+                <input type="text" autoComplete="off" id="wordInp" placeholder="Enter A Word" spellCheck="false" autoFocus onChange={handleChange} />
                 <div className='button-and-icon' disabled={isLoading} onClick={searchWord}>
-                    <button className="submitBtn" type="submit">Serach</button>
+                    <button className="submitBtn" type="submit">Search</button>
                     <VscSearch className='search-icon' />
                 </div>
             </form>
 
             {isLoading && <Loader />}
+            {errorMessage && <h2 className='error'>{errorMessage}</h2>}
 
             {
-                wordInfo && !isLoading && <>
+                wordInfo && !isLoading && !errorMessage && <>
                     <WordDetails wordInfo={wordInfo} />
                     <Meanings meanings={wordInfo[0].meanings} />
                 </>
